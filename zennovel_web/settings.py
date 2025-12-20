c11053b1708b6acb9bdb6a8007924f10c3c27d50
Dafit Fernandus <dafit.fernandus290304@gmail.com>
2025-12-20 22:49:45 +0700

    new

--- zennovel_web/settings.py
+++ zennovel_web/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Di settings.py
 CORS_ALLOW_ALL_ORIGINS = False # Matikan all origins
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
     "http://localhost:5173", # Ganti dengan origin frontend lokal Anda
     "https://zennovel.netlify.app",
 ]
